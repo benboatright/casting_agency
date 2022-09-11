b14dd4fe3e8507df1d9dc32441ab9b4e3e9dc4ad
Ben <benboatright@gmail.com>
2022-09-11 16:05:20 -0400

    add manager.py

---
+++ manage.py
@@ -0,0 +1,14 @@
+#9/11/22 #followed the manage.py from FSND #https://github.com/udacity/FSND/blob/master/projects/capstone/heroku_sample/starter/manage.py
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from app import app 
+from models import db 
+
+migrate = Migrate(app,db)
+
+manager = Manager(app)
+
+manager.add_command('db',MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
