bf2048f453215d280c95655df775ac712d5cc536
Ben <benboatright@gmail.com>
2022-09-11 15:38:39 -0400

    add migrations

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 #8/29/22 #Referenced Caryn's 'models.py' file to build this script unless noted otherwise #https://learn.udacity.com/nanodegrees/nd0044/parts/cd0037/lessons/905d1c8e-34d6-4d06-aaee-8ee91f041bc2/concepts/dc060384-b508-4a13-839e-01b636105556
 from sqlalchemy import Column,Integer,String, DateTime #8/29/22 #Referenced Amy's lesson for datatypes #https://learn.udacity.com/nanodegrees/nd0044/parts/cd0046/lessons/b957ba99-1c62-471c-8482-c18ac3d7943b/concepts/61e32678-d4aa-42f8-9767-050fef0ce9de
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #9/5/22 #referenced the code in the blog to hide secrets #https://dev.to/jakewitcher/using-env-files-for-environment%20#%20-variables-in-python-applications-55a1
 from dotenv import load_dotenv
@@ -17,7 +18,8 @@ def setup_db(app,uri):
     app.config['SQLALCHEMY_DATABASE_URI'] = uri #8/29/22 #Referenced Amy's lesson on connecting to database #https://learn.udacity.com/nanodegrees/nd0044/parts/cd0046/lessons/b957ba99-1c62-471c-8482-c18ac3d7943b/concepts/b2093f89-9b28-4d97-a02c-a829315fd3e1
     db.app = app
     db.init_app(app)
-    db.create_all()
+    migrate = Migrate(app,db) #9/11/22 #followed this video to setup migrations #https://learn.udacity.com/nanodegrees/nd0044/parts/cd0046/lessons/e01a0ac9-68dd-4844-b399-b8fa3be667e0/concepts/a1a5f8a9-37de-48e3-906c-3279599175b8
+    #db.create_all() #9/11/22 #removed this based on the video here #https://learn.udacity.com/nanodegrees/nd0044/parts/cd0046/lessons/e01a0ac9-68dd-4844-b399-b8fa3be667e0/concepts/336feaba-059e-4cd5-800b-3710cac58ce1
 
 class Actors(db.Model):
     __tablename__ = 'actors'
