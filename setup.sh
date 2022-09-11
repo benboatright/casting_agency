78cde9826024202dfa689d8a1829463f1ab289b9
Ben <benboatright@gmail.com>
2022-09-11 15:03:20 -0400

    setup file

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
-
+# referenced the setup.sh from here #https://github.com/udacity/FSND/blob/master/projects/capstone/heroku_sample/starter/setup.sh
+export DATABASE_URL="postgresql://benjaminboatright@localhost:5432/casting_agency"
