94a0d7ed1c59b1bea3e659650bc7d199ba78dbf5
Ben <benboatright@gmail.com>
2022-09-11 16:50:06 -0400

    downgrade Flask

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #9/11/22 #followed the manage.py from FSND #https://github.com/udacity/FSND/blob/master/projects/capstone/heroku_sample/starter/manage.py
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from app import app 
+from app import app
 from models import db 
 
 migrate = Migrate(app,db)
